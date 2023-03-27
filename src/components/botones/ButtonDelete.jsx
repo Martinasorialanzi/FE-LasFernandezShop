b964299a56d2b29b1f2630cdf4aaae5d132cc5ac
martinasorialanzi <martinasorialanzi@gmail.com>
2023-03-27 15:13:37 -0300

    formato celdas tabla stock

---
+++ src/components/botones/ButtonDelete.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+const ButtonDelete = () => {
+    
+  return (
+    <>
+    <Button>Delete</Button>
+    </>
+  )
+}
+
+export default ButtonDelete
--- src/components/stock/TablaStock2.jsx
+++ src/components/stock/TablaStock2.jsx
@@ -1,6 +1,13 @@
 import React, { useState, useEffect, useMemo } from "react";
 import { GetProducts2 } from "../../api/GetProducts";
-import { Button, FormCheck, Table,Form, DropdownButton,Dropdown } from "react-bootstrap";
+import {
+  Button,
+  FormCheck,
+  Table,
+  Form,
+  DropdownButton,
+  Dropdown,
+} from "react-bootstrap";
 import {
   Column,
   useTable,
@@ -8,16 +15,15 @@ import {
   useSortBy,
   usePagination,
 } from "react-table";
+import ButtonDelete from "../botones/ButtonDelete";
 
 const TablaStock = () => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-
   const getProducts = async (page) => {
     setIsLoading(true);
     const response = await GetProducts2();
-    console.log(response);
     setData(response.totalProducts);
 
     setIsLoading(false);
@@ -29,18 +35,41 @@ const TablaStock = () => {
       { Header: "PRENDA", accessor: "prenda" },
       { Header: "MARCA", accessor: "marca" },
       { Header: "CATEGORIA", accessor: "categoria" },
-      { Header: "TALLE", accessor: "", Cell: "" },
+      {
+        Header: "TALLE",
+        accessor: (t) => (
+          <>
+            {t.talle
+              ? t.talle
+              : t.prenda.includes("TALLE")
+              ? t.prenda.substring(t.prenda.length - 8).substring(6)
+              : null}
+          </>
+        ),
+      },
       { Header: "CLIENTE", accessor: "cliente" },
       {
         Header: "FECHA DE INGRESO",
         accessor: "fechaIngreso",
         Cell: ({ value }) => <>{value.substring(0, 10)}</>,
       },
-      { Header: "PRECIO DE VENTA", accessor: "precioVenta" },
-      { Header: "GANANCIA CLIENTE", Cell: "" },
-      { Header: "GANCANIA FERNANDEZ SHOP", accessor: "", Cell: "" },
+      {
+        Header: "PRECIO DE VENTA",
+        accessor: (p) => `$ ${p.precioVenta}`,
+        id: "precio",
+      },
+      {
+        Header: "GANANCIA CLIENTE",
+        accessor: (d) => `$ ${Math.ceil(d.precioVenta * 0.7)}`,
+        id: "ganancia",
+      },
+      {
+        Header: "GANCANIA FERNANDEZ SHOP",
+        accessor: (d) => `$ ${Math.ceil(d.precioVenta * 0.3)}`,
+      },
       { Header: "ESTADO", accessor: "estado" },
-      { Header: "TIEMPO EN VENTA", accessor: "tiempoEnVenta" },
+      { Header: "TIEMPO EN VENTA", accessor: "tiempoEnVenta", cell: "chau" },
+      { Header: "TIEMPO ", Cell: <><ButtonDelete/></>, },
     ],
     []
   );
@@ -51,18 +80,19 @@ const TablaStock = () => {
       data,
       autoResetHiddenColumns: false, //  <-- stops the rerendering
       autoResetSortBy: false, //  <-- stops the rerendering
+      initialState: { pageSize: 50 },
     },
     useGlobalFilter,
     useSortBy,
     usePagination
   );
 
-  
   const {
     canPreviousPage,
     canNextPage,
     getTableProps, //funcion que nos devuelve las propiedades que va a recibir la etiqueta table
     getTableBodyProps,
+    getCellProps,
     headerGroups, //array que contiene cada uno d elos grupos del header
     footerGroups,
     // rows, //array que contiene lo que va en cada fila (la data)
@@ -79,10 +109,9 @@ const TablaStock = () => {
     setPageSize,
     allColumns,
     getToggleHideAllColumnsProps,
-    
   } = tableInstance;
-  const {pageIndex, pageSize}=state
-  
+  const { pageIndex, pageSize } = state;
+
   useEffect(() => {
     getProducts();
   }, []);
@@ -95,35 +124,32 @@ const TablaStock = () => {
           value={state.globalFilter}
           onChange={(e) => setGlobalFilter(e.target.value)}
         ></input>
-        <p>Cantidad de filas encontradas: {preGlobalFilteredRows.length}</p>
+        {/* <p>Cantidad de filas encontradas: {preGlobalFilteredRows.length}</p> */}
       </div>
       <div>
         {/* <DropdownButton id="dropdown-basic-button" title="Dropdown button">    */}
         {/* <Dropdown.Item> */}
         <div>
-      <Form.Check 
+          <Form.Check
             type="checkbox"
-            label={'Toggle All'}
+            label={"Toggle All"}
             {...getToggleHideAllColumnsProps()}
-          /> 
-          </div>
-          {/* </Dropdown.Item> */}
-          <div>
-            {allColumns.map((column)=>(
-              // <Dropdown.Item  key={column.id}>
-             <div>
-             <label>
-              <input
-                   type="checkbox"
-                   {...column.getToggleHiddenProps()}
-                 /> 
-                 {column.Header}
-                 </label>
-                 </div>
-                 /* </Dropdown.Item> */
-            ))}
-          </div>
-          {/* </DropdownButton> */}
+          />
+        </div>
+        {/* </Dropdown.Item> */}
+        <div>
+          {allColumns.map((column) => (
+            // <Dropdown.Item  key={column.id}>
+            <div key={column.id}>
+              <label>
+                <input type="checkbox" {...column.getToggleHiddenProps()} />
+                {column.Header}
+              </label>
+            </div>
+            /* </Dropdown.Item> */
+          ))}
+        </div>
+        {/* </DropdownButton> */}
       </div>
       <Table striped bordered hover className="container " {...getTableProps}>
         <thead>
@@ -171,27 +197,41 @@ const TablaStock = () => {
 
       <div>
         <span>
-          Page{''}
-          <strong>{pageIndex+1} of {pageOptions.length}</strong>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>
         </span>
-        <button onClick={()=>gotoPage(0)} disabled={!canPreviousPage}>{'<<'}</button>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>
         <button onClick={() => previousPage()} disabled={!canPreviousPage}>
           {"<"}
         </button>
-        
-      <input type="number" defaultValue={pageIndex+1} 
-       onChange={(e)=>{
-        const pageNumber=e.target.value ? Number(e.target.value) -1 :0
-        gotoPage(pageNumber)
-       }} />
+
+        <input
+          type="number"
+          value={pageIndex + 1}
+          min={1}
+          max={pageCount}
+          onChange={(e) => {
+            const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0;
+            gotoPage(pageNumber);
+          }}
+        />
 
         <button onClick={() => nextPage()} disabled={!canNextPage}>
           {">"}
         </button>
-        <button onClick={()=>gotoPage(pageCount-1)} disabled={!canNextPage}>{'>>'}</button>
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
+        </button>
 
-        <select value={pageSize} onChange={(e)=>setPageSize(Number(e.target.value))}>
-          {[25,50,75,100].map((pageSize)=>(
+        <select
+          value={pageSize}
+          onChange={(e) => setPageSize(Number(e.target.value))}
+        >
+          {[25, 50, 75, 100].map((pageSize) => (
             <option key={pageSize} value={pageSize}>
               Mostrar {pageSize}
             </option>
